1a5d80b683e9a5c9cbb6a8e529bcda41c8ae7415
dgnnnnnnnnn <yelims@kakao.com>
2023-09-12 11:34:47 +0900

    w2_second

---
+++ w2_1/0912vcpp/0912vcpp/0912vcpp.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    string i;
+
+    cout << "12341234" << endl << "정대건님의" << endl << "Application" << endl;
+    cin >> i;
+    // p, f, j
+
+    int count = 0;
+
+    while (true) {
+        count++;
+        int a;
+        cin >> a;
+        if (a == 7) {
+            break;
+        }
+    }
+    cout << "a : " << count << endl;
+
+
+    /* if (i == "p") {
+         cout << "나는 과제를 잘하는 학생이다";
+     }
+     else if (i == "f") {
+         cout << "나는 과제가 너무 즐겁다";
+     }
+     else if (i == "j") {
+         cout << "교수님 과제 더 내주세요";
+     } */
+
+}
