e45bd4d48d7d7ffdd1e37b5221a33b1a2499cc63
dgnnnnnnnnn <yelims@kakao.com>
2023-09-26 11:08:47 +0900

    0926

---
+++ w2_1/0912vcpp/0912vcpp/test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <conio.h>
+#include <vector>
+#include <chrono>
+#include <thread>
+#include <string>
+using namespace std;
+
+
+int main() {
+
+	int i = _getch();
+	cout << i;
+}
--- w4_1/0926vcpp/Project1/commandwasd.cpp
+++ w4_1/0926vcpp/Project1/commandwasd.cpp
@@ -10,17 +10,20 @@ using namespace std;
 
 // 함수: 화살표 키를 화살표 기호로 변환
 string GetArrowSymbol(char inputKey) {
-    if (inputKey == 72) {
-        return "↑"; // 위쪽 화살표 키 (↑)
-    }
-    else if (inputKey == 80) {
-        return "↓"; // 아래쪽 화살표 키 (↓)
-    }
-    else if (inputKey == 75) {
-        return "←"; // 왼쪽 화살표 키 (←)
-    }
-    else if (inputKey == 77) {
-        return "→"; // 오른쪽 화살표 키 (→)
+    if (inputKey == 224) {
+        inputBuffer.push_back(inputKey);
+        if (inputKey == 72) {
+            return "↑"; // 위쪽 화살표 키 (↑)
+        }
+        else if (inputKey == 80) {
+            return "↓"; // 아래쪽 화살표 키 (↓)
+        }
+        else if (inputKey == 75) {
+            return "←"; // 왼쪽 화살표 키 (←)
+        }
+        else if (inputKey == 77) {
+            return "→"; // 오른쪽 화살표 키 (→)
+        }
     }
     else
         return ""; // 다른 키인 경우 빈 문자열 반환
@@ -54,3 +57,15 @@ int main() {
 
     return 0;
 }
+
+
+/*
+    WinMain 진입점 함수
+    int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow);
+
+    * hInstance : 인스턴스에 대한 핸들 이거나 모듈에 대한 핸들입니다. 
+                  운영 체제는 이 값을 사용하여 메모리에 로드될 때 실행 파일 또는 EXE를 식별합니다.
+    * hPrevInstance : 의미 X, 16비트 Windows에서 사용되었지만 이제는 항상 0입니다.
+    * pCmdLine : 명령줄 인수(main 함수에 전달 할 인수) 가 유니코드 문자열로 포함
+    * nCmdShow : 기본 애플리케이션 창 크기 조절
+*/
